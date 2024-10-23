828d603fd6718c7917d1cf1d6fa0e1bc82cdc957
Adrien <print@polyscribe.fr>
2024-10-23 16:08:50 +0200

    netlify.toml

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -6,6 +6,7 @@ import uglify from 'gulp-uglify';
 import concat from 'gulp-concat';
 import rename from 'gulp-rename';
 import autoprefixer from 'gulp-autoprefixer';
+import fs from 'fs';
 
 // Configurez gulp-sass pour utiliser dart-sass
 const sassCompiler = gulpSass(sass);
@@ -15,6 +16,7 @@ const bs = browserSync.create();
 
 // Compile SCSS into CSS
 gulp.task('css', function() {
+  console.log('Running CSS task');
   return gulp.src('./scss/*.scss')
     .pipe(sassCompiler().on('error', sassCompiler.logError))
     .pipe(autoprefixer({
@@ -27,6 +29,7 @@ gulp.task('css', function() {
 
 // Minify and concatenate JS
 gulp.task('js', function() {
+  console.log('Running JS task');
   return gulp.src('./js/*.js')
     .pipe(concat('main.js'))
     .pipe(uglify())
@@ -35,23 +38,39 @@ gulp.task('js', function() {
     .pipe(bs.stream());
 });
 
+// Copy HTML files to dist
+gulp.task('html', function() {
+  console.log('Running HTML task');
+  return gulp.src('./*.html')
+    .pipe(gulp.dest('./dist'))
+    .pipe(bs.stream());
+});
+
+// Create _redirects file in dist
+gulp.task('redirects', function(done) {
+  console.log('Creating _redirects file');
+  fs.writeFileSync('./dist/_redirects', '/* /index.html 200');
+  done();
+});
+
 // Static server + watching scss/js/html files
 gulp.task('browserSync', function() {
   bs.init({
     server: {
-      baseDir: './'
+      baseDir: './dist'
     }
   });
 });
 
 // Build task
-gulp.task('build', gulp.series('css', 'js'));
+gulp.task('build', gulp.series('css', 'js', 'html', 'redirects'));
 
 // Dev task
 gulp.task('dev', gulp.series('build', 'browserSync', function() {
   gulp.watch('./scss/*.scss', gulp.series('css'));
   gulp.watch('./js/*.js', gulp.series('js'));
-  gulp.watch('./*.html').on('change', bs.reload);
+  gulp.watch('./*.html', gulp.series('html'));
+  gulp.watch('./dist/*.html').on('change', bs.reload);
 }));
 
 // Default task
