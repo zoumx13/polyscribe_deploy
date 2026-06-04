2043594510a12887104d1cf8165e5ff4bce49140
zoumx13 <114163526+zoumx13@users.noreply.github.com>
2026-06-04 18:17:14 +0200

    Add files via upload

---
+++ gulpfile.mjs
@@ -0,0 +1,153 @@
+import gulp from 'gulp';
+import * as sass from 'sass';
+import gulpSass from 'gulp-sass';
+import browserSync from 'browser-sync';
+import uglify from 'gulp-uglify';
+import concat from 'gulp-concat';
+import rename from 'gulp-rename';
+import autoprefixer from 'gulp-autoprefixer';
+import cleanCSS from 'gulp-clean-css';
+import fs from 'fs';
+
+// Configurez gulp-sass pour utiliser dart-sass
+const sassCompiler = gulpSass(sass);
+
+// Créez une instance de browserSync
+const bs = browserSync.create();
+
+// Compile SCSS into CSS
+gulp.task('scss', function() {
+  console.log('Running SCSS task');
+  return gulp.src('./scss/*.scss')
+    .pipe(sassCompiler().on('error', sassCompiler.logError))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./css'))
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./css'))
+    .pipe(bs.stream());
+});
+
+// Copy CSS files to dist
+gulp.task('css', function() {
+  console.log('Running CSS task');
+  return gulp.src('./css/*.css')
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(bs.stream());
+});
+
+// Minify and concatenate JS
+gulp.task('js', function() {
+  console.log('Running JS task');
+  return gulp.src('./js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(bs.stream());
+});
+
+// Copy HTML files to dist
+gulp.task('html', function() {
+  console.log('Running HTML task');
+  return gulp.src('./*.html')
+    .pipe(gulp.dest('./dist'))
+    .pipe(bs.stream());
+});
+
+// Copy vendor files to dist
+gulp.task('vendor', function() {
+  console.log('Running Vendor task');
+  return gulp.src('./vendor/**/*')
+    .pipe(gulp.dest('./dist/vendor'))
+    .pipe(bs.stream());
+});
+
+// Copy image files to dist
+gulp.task('images', function() {
+  console.log('Running Images task');
+  return gulp.src('./img/**/*')
+    .pipe(gulp.dest('./dist/img'))
+    .pipe(bs.stream());
+});
+
+// Générer sitemap.xml dynamiquement avec la date du jour
+gulp.task('seo', function(done) {
+  const fs2 = fs;
+  const today = new Date().toISOString().split('T')[0];
+  
+  const pages = [
+    {url:'',         freq:'weekly',  prio:'1.0'},
+    {url:'etudiants.html',                          freq:'monthly', prio:'0.9'},
+    {url:'impression-memoire-marseille.html',        freq:'monthly', prio:'0.9'},
+    {url:'plans-architecte-marseille.html',          freq:'monthly', prio:'0.85'},
+    {url:'externalisation-impression-marseille.html',freq:'monthly', prio:'0.85'},
+    {url:'reprographie-marseille.html',              freq:'monthly', prio:'0.8'},
+    {url:'impression-affiche-marseille.html',        freq:'monthly', prio:'0.75'},
+    {url:'impression-flyer-marseille.html',          freq:'monthly', prio:'0.75'},
+    {url:'location-salle-marseille.html',            freq:'monthly', prio:'0.7'},
+    {url:'architectes.html',                         freq:'monthly', prio:'0.7'},
+    {url:'avocats.html',                             freq:'monthly', prio:'0.65'},
+    {url:'associations.html',                        freq:'monthly', prio:'0.65'},
+    {url:'autoentrepreneurs.html',                   freq:'monthly', prio:'0.65'},
+  ];
+
+  const BASE = 'https://www.polyscribe.fr/';
+  const urls = pages.map(p => `  <url>
+    <loc>${BASE}${p.url}</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>${p.freq}</changefreq>
+    <priority>${p.prio}</priority>
+  </url>`).join('\n');
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+${urls}
+</urlset>`;
+
+  fs2.writeFileSync('./dist/sitemap.xml', xml);
+  fs2.copyFileSync('./robots.txt', './dist/robots.txt');
+  console.log('SEO files generated with date: ' + today);
+  done();
+});
+
+// Create _redirects file in dist (SPA fallback, exclut les fichiers SEO)
+gulp.task('redirects', function(done) {
+  console.log('Creating _redirects file');
+  const rules = [
+    '/sitemap.xml  /sitemap.xml  200',
+    '/robots.txt   /robots.txt   200',
+    '/*            /index.html   200'
+  ].join('\n');
+  fs.writeFileSync('./dist/_redirects', rules);
+  done();
+});
+
+// Static server + watching scss/js/html files
+gulp.task('browserSync', function() {
+  bs.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+});
+
+// Build task
+gulp.task('build', gulp.series('scss', 'css', 'js', 'html', 'vendor', 'images', 'seo', 'redirects'));
+
+// Dev task
+gulp.task('dev', gulp.series('build', 'browserSync', function() {
+  gulp.watch('./scss/*.scss', gulp.series('scss', 'css'));
+  gulp.watch('./js/*.js', gulp.series('js'));
+  gulp.watch('./*.html', gulp.series('html'));
+  gulp.watch('./vendor/**/*', gulp.series('vendor'));
+  gulp.watch('./img/**/*', gulp.series('images'));
+  gulp.watch('./dist/*.html').on('change', bs.reload);
+}));
+
+// Default task
+gulp.task('default', gulp.series('dev'));
